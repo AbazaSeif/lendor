ceea1164a81b7220b9551ed50cb04c753e4b0123
Zachariah T. Dzielinski <zachdzielinski@gmail.com>
2016-07-21 00:54:12 -0400

    Added very basic login, logout, and welcome username page

--- functions.php
+++ functions.php
@@ -1,12 +1,28 @@
 <?php
 
 // Check if the user is logged in
-// Returns a user mode if they are
+// Returns a user model if they are
 // Returns false if they are not
 // Returns false on any error
 function logged_in () {
     try {
         return User::findOrFail($_SESSION["user_id"]);
+    } catch (Exception $e) {}
+    return false;
+}
+
+// Check if login credentials are valid
+// Requires a parameters array
+// Returns user model on valid authentication
+// Returns false on invalid authentication
+function valid_login ($params) {
+    try {
+        $username = $params["username"];
+        $password = $params["password"];
+        $user = User::where("username", "=", $username)->firstOrFail();
+        if (password_verify($password, $user->password)) {
+            return $user;
+        }
     } catch (Exception $e) {}
     return false;
 }
--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 require "vendor/autoload.php";
 require "config.php";
 require "functions.php";
-// Set up everything
 require "setup.php";
 
 // Usage aliases
@@ -21,15 +20,38 @@ use \Slim\Views\Twig as Twig;
 // Index route
 $app->get("/", function ($req, $res, $args) {
     // Only allow users that are logged in
-    if (!logged_in()) {
+    if (!($user = logged_in())) {
         // Redirect to the login page
         return $res->withHeader("Location", "/login");
     }
+    // Draw the index page 
+    return $this->view->render($res, "index.html", ["username" => $user->username]);
 });
 
 // Login route
-$app->get("/login", function ($req, $res, $args) {
+$app->any("/login", function ($req, $res, $args) {
+    // If user is logged in
+    if (logged_in()) {
+        // No need to log in twice, redirect to index
+        return $res->withHeader("Location", "/");
+    }
+    // Store user or false on valid login check
+    if ($user = valid_login($req->getParsedBody())) {
+        // On valid login, set user id token
+        $_SESSION["user_id"] = $user->id;
+        // Redirect to index
+        return $res->withHeader("Location", "/");
+    }
+    // User not authenticated, redraw the login page
     return $this->view->render($res, "login.html", []);
+});
+
+// Logout rout
+$app->get("/logout", function ($req, $res, $args) {
+    // Unset the user id token
+    unset($_SESSION["user_id"]);
+    // Redirect to the login page
+    return $res->withHeader("Location", "/login");
 });
 
 // Run slim
